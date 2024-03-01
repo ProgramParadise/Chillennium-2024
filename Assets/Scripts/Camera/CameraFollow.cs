478ec5a7b83fa293b2eb87f4122868d5c061ac5f
ProgramParadise <contact.programparadise@gmail.com>
2024-03-01 16:10:01 -0600

    Final update pre-chillennium

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -4,8 +4,11 @@ using System;
 
 public class CameraFollow : MonoBehaviour
 {
-    [HideInInspector]
     public GameObject player;
+    public bool moveX = true;
+    public bool moveY = true;
+    public float slowX = 1;
+    public float slowY = 1;
     float playerInsantiateDelay = 2f; // How long to check again if the player object exists after it was not found the first time
     public Vector3 last_position;
     public bool playingCutscene = false;
@@ -33,7 +36,16 @@ public class CameraFollow : MonoBehaviour
         {
             Vector3 lookpoint = player.transform.position - last_position;
             Vector2 error = (lookpoint * 20) + player.transform.position - gameObject.transform.position;
-            gameObject.transform.Translate(error.x / 10, error.y / 10, 0);
+            if (moveX && moveY)
+            {
+                gameObject.transform.Translate((error.x / 10) * slowX, (error.y / 10) * slowY, 0);
+            } else if (moveX && !moveY)
+            {
+                gameObject.transform.Translate((error.x / 10) * slowX, 0, 0);
+            } else
+            {
+                gameObject.transform.Translate(0, (error.y / 10) * slowY, 0);
+            }
             last_position = player.transform.position;
         }
     }
--- Assets/Scripts/Player Controllers/PlayerMovement.cs
+++ Assets/Scripts/Player Controllers/PlayerMovement.cs
@@ -6,24 +6,50 @@ using UnityEngine;
 public class Player_Movement : MonoBehaviour
 {
     public bool playerCanMove = true;
+    public bool playerCanMoveVertical = true;
+    public bool playerCanJump = false;
+    public KeyCode userKey = KeyCode.Space;
     public string axesNumber = "1";
-    private Rigidbody2D rb;
+    public Rigidbody2D rb;
     private PlayerStats stats;
     public Animator animator;
     float moveHorizontal;
     float moveVertical;
+    public float buttonTime = 0.5f;
+    public float jumpHeight = 5;
+    public float cancelRate = 100;
+    float jumpTime;
+    bool jumping;
+    bool jumpCancelled;
+
+    public LayerMask groundLayer;
+
+    bool IsGrounded()
+    {
+        Vector2 position = transform.position;
+        Vector2 direction = Vector2.down;
+        float distance = 1.0f;
+
+        RaycastHit2D hit = Physics2D.Raycast(position, direction, distance, groundLayer);
+        if (hit.collider != null)
+        {
+            Debug.Log("Grounded");
+            return true;
+        }
+        Debug.Log("Not Grounded");
+        return false;
+    }
 
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
         stats = gameObject.GetComponent<PlayerStats>();
+        if (playerCanJump) playerCanMoveVertical = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-
-
         if (moveHorizontal > 0.01)
         {
             gameObject.transform.eulerAngles = new Vector3(0, 180, 0);
@@ -32,21 +58,66 @@ public class Player_Movement : MonoBehaviour
         {
             gameObject.transform.eulerAngles = new Vector3(0, 0, 0);
         }
+        if (playerCanJump)
+        {
+            if (Input.GetKeyDown(userKey) && IsGrounded())
+            {
+                float jumpForce = Mathf.Sqrt(jumpHeight * -2 * (Physics2D.gravity.y * rb.gravityScale));
+                rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
 
+                jumping = true;
+                jumpCancelled = false;
+                jumpTime = 0;
+            }
+
+            if (jumping)
+            {
+                jumpTime += Time.deltaTime;
+                if (Input.GetKeyUp(userKey))
+                {
+                    jumpCancelled = true;
+                }
+
+                if (jumpTime > buttonTime)
+                {
+                    jumping = false;
+                }
+            }
+        } 
 
         animator.SetFloat("Speed", rb.velocity.magnitude);
-
     }
 
     private void FixedUpdate()
     {
         moveHorizontal = Input.GetAxis("Horizontal" + axesNumber);
-        moveVertical = Input.GetAxis("Vertical" + axesNumber);
-
-        rb.velocity = new Vector2(moveHorizontal * stats.speed, moveVertical * stats.speed);
-        if (rb.velocity.magnitude > stats.speed)
+        if (playerCanMoveVertical)
         {
-            rb.velocity = rb.velocity * (stats.speed / rb.velocity.magnitude);
+            moveVertical = Input.GetAxis("Vertical" + axesNumber);
         }
+        else moveVertical = 0f;
+
+        if (playerCanJump)
+        {
+            if (jumpCancelled && jumping && rb.velocity.y > 0)
+            {
+                rb.AddForce(Vector2.down * cancelRate);
+            }
+        }
+
+        if (playerCanMoveVertical)
+        {
+            rb.velocity = new Vector2(moveHorizontal * stats.speed, moveVertical * stats.speed);
+            if (rb.velocity.magnitude > stats.speed)
+            {
+                rb.velocity = rb.velocity * (stats.speed / rb.velocity.magnitude);
+            }
+        }
+        else
+        {
+            rb.velocity = new Vector2(moveHorizontal * stats.speed, rb.velocity.y);
+        }
+
+        
     }
 }
